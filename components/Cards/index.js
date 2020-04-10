1241e5822f335f067bca9fe6d90e238b10d329dd
joshua williams <joshua@joshuas-MBP.fios-router.home>
2020-04-10 12:31:53 -0400

    finished MVP working on stretch

--- components/Cards/index.js
+++ components/Cards/index.js
@@ -4,9 +4,49 @@
 // Study the response data you get back, closely.
 // You will be creating a card for each 'article' in the response.
 // This won't be as easy as just iterating over an array though.
-//
+let articleSec = document.querySelector('.cards-container')
+function getCard(){
+axios.get('https://lambda-times-backend.herokuapp.com/articles')
+    .then(res=>{
+        res.data.articles.javascript.forEach(item => articleSec.appendChild(CreateCard(item)))
+
+    })
+}
 // Write a function that returns the following markup:
-//
+function CreateCard(obj){
+    let card = document.createElement('div')
+    let headline = document.createElement('div')
+    let author = document.createElement('div')
+    let imgContainer = document.createElement('div')
+    let image = document.createElement('img')
+    let name = document.createElement('span')
+
+    // Content & Classes
+    card.classList.add('card')
+    headline.classList.add('headline')
+    author.classList.add('author')
+    imgContainer.classList.add('img-container')
+
+
+    headline.textContent = obj.headline
+    image.src = obj.authorPhoto
+    image.alt = 'Photo of the author'
+    name.textContent =`By ${obj.authorName}`
+
+    // structure
+    card.appendChild(headline)
+    card.appendChild(author)
+    imgContainer.appendChild(image)
+    card.appendChild(imgContainer)
+    card.appendChild(name)
+
+    
+
+    return card
+
+
+}
+getCard();
 // <div class="card">
 //   <div class="headline">{Headline of article}</div>
 //   <div class="author">
