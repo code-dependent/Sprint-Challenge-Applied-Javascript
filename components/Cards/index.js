4547833fe09179dedea5f893ca3e32b446ee2f25
joshua williams <joshua@joshuas-MBP.fios-router.home>
2020-04-10 16:50:08 -0400

    working on sprint for fun

--- components/Cards/index.js
+++ components/Cards/index.js
@@ -1,26 +1,22 @@
-// STEP 3: Create article cards.
-// -----------------------
-// Send an HTTP GET request to the following address: https://lambda-times-backend.herokuapp.com/articles
-// Study the response data you get back, closely.
-// You will be creating a card for each 'article' in the response.
-// This won't be as easy as just iterating over an array though.
-let articleSec = document.querySelector('.cards-container')
+
+let articleSection = document.querySelector('.cards-container')
+
 function getCard(){
 axios.get('https://lambda-times-backend.herokuapp.com/articles')
     .then(res=>{
         console.log(res)
-        let obj = res.data.articles
-        obj.javascript.forEach(item => articleSec.appendChild(CreateCard(item)))
-        obj.bootstrap.forEach(item => articleSec.appendChild(CreateCard(item)))
-        obj.technology.forEach(item => articleSec.appendChild(CreateCard(item)))
-        obj.jquery.forEach(item => articleSec.appendChild(CreateCard(item)))
-        obj.node.forEach(item => articleSec.appendChild(CreateCard(item)))
-
+        let catagories = Object.keys(res.data.articles)
+        console.log(catagories)
+        catagories.forEach(catagory => {
+            let articleData = res.data.articles[`${catagory}`];
+            return articleData.forEach(articleObject=>articleSection.appendChild(CreateCard(articleObject)))
+        })
     })
 }
-// Write a function that returns the following markup:
+
+
 function CreateCard(obj){
-    let card = document.createElement('div')
+    let cardParent = document.createElement('div')
     let headline = document.createElement('div')
     let author = document.createElement('div')
     let imgContainer = document.createElement('div')
@@ -28,7 +24,7 @@ function CreateCard(obj){
     let name = document.createElement('span')
 
     // Content & Classes
-    card.classList.add('card')
+    cardParent.classList.add('card')
     headline.classList.add('headline')
     author.classList.add('author')
     imgContainer.classList.add('img-container')
@@ -43,24 +39,13 @@ function CreateCard(obj){
     imgContainer.appendChild(image)
     author.appendChild(imgContainer)
     author.appendChild(name)
-    card.appendChild(headline)
-    card.appendChild(author)
+    cardParent.appendChild(headline)
+    cardParent.appendChild(author)
 
     
 
-    return card
+    return cardParent
 
 
 }
 getCard();
-// <div class="card">
-//   <div class="headline">{Headline of article}</div>
-//   <div class="author">
-//     <div class="img-container">
-//       <img src={url of authors image} />
-//     </div>
-//     <span>By {author's name}</span>
-//   </div>
-// </div>
-//
-// Use your function to create a card for each of the articles and add the card to the DOM.
