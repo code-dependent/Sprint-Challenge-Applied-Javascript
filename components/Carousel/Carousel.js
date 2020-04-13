e9f58010cf8ee92a6661544e704e4f503a2c49c5
joshua williams <joshua@joshuas-MBP.fios-router.home>
2020-04-13 10:32:48 -0400

    finished stretch with extra personal stretch commented out

--- components/Carousel/Carousel.js
+++ components/Carousel/Carousel.js
@@ -1,50 +1,62 @@
-/* If You've gotten this far, you're on your own! Although we will give you some hints:
-    1. You will need to write a function that creates the carousel component, you will find the HTML below.
-    2. You will need to grab a reference to all of the images
-    3. Create a current index
-    4. Those buttons are gonna need some click handlers.
-    5. Think of how you would animate this component. Make the cards slide in and out, or fade. It's up to you!
-    6. Have fun!
-*/
-let carouselContainer = document.querySelector('.carousel-container')
 
+let carouselContainer = document.querySelector('div.carousel-container')
+
+// function getURL(){
+//   axios.get('https://lambda-times-backend.herokuapp.com/articles')
+//   .then(res=>{
+//     let catagories = Object.keys(res.data.articles)
+//     let testar = []
+//     console.log(catagories)
+
+//     catagories.forEach(catagory=>{res.data.articles[`${catagory}`].forEach(item=>testar.push(item.authorPhoto))})
+//     testar = [... new Set(testar)]
+//     console.log(testar)
+//     return carouselContainer.appendChild(carousel(testar))
+//   })
+// }
 function carousel(){
-  let imgUrls = ["./assets/carousel/mountains.jpeg",
+  const imgUrls = ["./assets/carousel/mountains.jpeg",
                 "./assets/carousel/computer.jpeg",
                 "./assets/carousel/trees.jpeg",
                 "./assets/carousel/turntable.jpeg"]
 
 
-  let carousel = document.createElement('div')
+  const carousel = document.createElement('div')
   carousel.classList.add('carousel')
-  let buttonL = document.createElement('div')
+  const buttonL = document.createElement('div')
   buttonL.classList.add('left-button')
   buttonL.textContent = '<'
-  let buttonR = document.createElement('div')
+  const buttonR = document.createElement('div')
   buttonR.classList.add('right-button')
   buttonR.textContent = '>'
   carousel.appendChild(buttonL)
-  
-  let img1 = document.createElement('img')
+  const img = document.createElement('img')
+  carousel.appendChild(img)
 
-  imgUrls.forEach(url=>{
-    let img = document.createElement('img')
-    img.src = url
-    
-    carousel.appendChild(img)
-    })
+  let track = 0
+  img.src = imgUrls[track]
+  
+  buttonR.addEventListener('click', () =>{
+    if(track === imgUrls.length -1){
+      track = 0
+    }else{
+      track++
+    }
+    return img.src = imgUrls[track]
+  })
+
+  buttonL.addEventListener('click', () =>{
+    if(track === 0){
+      track = imgUrls.length -1
+    }else{
+      track--
+    }
+    img.src = imgUrls[track]
+  })
+
     carousel.appendChild(buttonR)
-    console.log(carousel)
     return carousel
 }
 carouselContainer.appendChild(carousel())
-/* HTML:
-  <div class="carousel">
-    <div class="left-button"> < </div>
-    <img src="./assets/carousel/mountains.jpeg" />
-    <img src="./assets/carousel/computer.jpeg" />
-    <img src="./assets/carousel/trees.jpeg" />
-    <img src="./assets/carousel/turntable.jpeg" />
-    <div class="right-button"> > </div>
-  </div>
-*/
+
+
